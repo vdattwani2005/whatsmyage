e639451dfa1033b7d0ab48cd120a64b4d221035b
Vansh Dattwani <vdattwani2005@gmail.com>
2018-05-02 21:04:59 +0530

    added age calculator

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 # [START app]
 import logging
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import DateField
 import datetime
-from flask import Flask
-
 
 app = Flask(__name__)
+app.secret_key = 'SHH!'
 
 
-@app.route('/')
-def hello():
-    return 'Hello World, Vansh!. ' + str(datetime.datetime.now())
+class DateForm(FlaskForm):
+    dt = DateField('Pick a Date', format="%d/%m/%Y")
+
+
+@app.route('/', methods=['post','get'])
+def home():
+    form = DateForm()
+    if form.validate_on_submit():
+        dob = form.dt.data.strftime('%d/%m/%Y')
+        age = datetime.datetime.now()-datetime.datetime(form.dt.data.year,form.dt.data.month,form.dt.data.day)
+        return "Your birth date is " + dob + "<br> Your age is " + str(age.days) + " days."
+    return render_template('index.html', form=form)
 
 
 @app.errorhandler(500)
